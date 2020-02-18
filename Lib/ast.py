a0e70854eb19b2126499f413a80b49a6b780b7fe
Richard <qpotizo@gmail.com>
2020-02-17 20:18:15 -0500

    dump ast as json string

--- Lib/ast.py
+++ Lib/ast.py
@@ -163,6 +163,35 @@ def dump(node, annotate_fields=True, include_attributes=False, *, indent=None):
     return _format(node)[0]
 
 
+def dump_json(node):
+    """
+    Return a string in json representation of the tree in node. This is mainly
+    useful for debugging purposes or perhaps serializing the tree.
+    """
+    def recurse_node_to_dict(obj): pass
+
+    def node_to_dict(cls):
+        return {cls.__class__.__name__: dict(
+            (key, recurse_node_to_dict(value))
+            for (key, value) in cls.__dict__.items()
+        )}
+
+    def recurse_node_to_dict(obj):
+        if isinstance(obj, list):
+            return list(map(node_to_dict, obj))
+        elif hasattr(obj, '__dict__'):
+            if len(obj.__dict__.items()):
+                return node_to_dict(obj)
+            else:
+                return obj.__class__.__name__
+        elif type(obj).__name__ == 'ellipsis':
+            return 'Ellipsis'
+        else:
+            return obj
+
+    return str(node_to_dict(node))
+
+
 def copy_location(new_node, old_node):
     """
     Copy source location (`lineno`, `col_offset`, `end_lineno`, and `end_col_offset`
